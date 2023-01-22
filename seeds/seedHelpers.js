b6bc40149e71d300135fe029ce8357e91571a9e9
BingNa Yang <bing334@gmail.com>
2023-01-22 11:59:59 -0500

    add seed files

---
+++ seeds/seedHelpers.js
@@ -0,0 +1,15 @@
+module.exports.name = [
+    'Louvre',
+    'National Museum of China',
+    'Nanjing Museum',
+    'Tate Modern',
+    'Vatican Museums',
+    'The British Museum',
+    'Natural History Museum',
+    'The National Gallery',
+    'The Metropolitan Museum of Art',
+    'State Hermitage Museum',
+    'Victoria and Albert Museum',
+    'National Gallery of Art',
+    'National Palace Museum'
+]
