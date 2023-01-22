bd2f6ec21bfd8f72e5a7ad176a5d2ca8d7fadc22
BingNa Yang <bing334@gmail.com>
2023-01-21 19:53:10 -0500

    Call mongoose connect  and create a new route for new museum

--- app.js
+++ app.js
@@ -1,12 +1,35 @@
 const express = require('express');
-const app = express();
 const path = require('path');
+const mongoose = require('mongoose');
+const Museum = require('./models/museum');
+
+// Call mongoose.connect
+mongoose.connect('mongodb://localhost:27017/discover-museum', {
+    // useNewUrlParser: true,
+    // useCreateIndex: true,
+    // useUnifiedTopology: true
+})
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "Connection Error:"));
+db.once("open", () =>{
+    console.log("Database connected");
+})
+
+const app = express();
 
 app.set('view engine','ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.get('/', (req, res) => {
     res.render('home');
+})
+
+// Creating a new museum
+app.get('/makemuseum', async (req, res) => {
+    const theMusuem = new Museum({title: 'MOMA', description: 'A place that fuels creativity, ignites minds, and provides inspiration'})
+    await theMusuem.save();
+    res.send(theMusuem);
 })
 
 app.listen(3000, () => {
