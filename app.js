ee8b2db8c981f58876f8c3379ceeeb9408b0f83b
BingNa Yang <bing334@gmail.com>
2023-01-23 20:15:05 -0500

    Museum New & Create
    - Create museums/new.ejs: to show form for new museum
    - Update museums/index.ejs: add link to redirect to add museum page
    - Update museums/show.ejs: add link to redirect to all museum page

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
 const Museum = require('./models/museum');
+const museum = require('./models/museum');
 
 // Call mongoose.connect
 mongoose.connect('mongodb://localhost:27017/discover-museum', {
@@ -20,6 +21,8 @@ const app = express();
 
 app.set('view engine','ejs');
 app.set('views', path.join(__dirname, 'views'));
+// parses incoming requests
+app.use(express.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => {
     res.render('home');
@@ -31,11 +34,24 @@ app.get('/museums', async (req, res) => {
     res.render('museums/index', {museums});
 })
 
+// New Route: add a museum page
+app.get('/museums/new', async (req, res) => {
+    res.render('museums/new');
+})
+
+// POST new museum
+app.post('/museums', async (req, res) => {
+    const theMuseum = new Museum(req.body.museum);
+    await theMuseum.save();
+    res.redirect(`/museums/${theMuseum._id}`)
+})
+
 // Show Route: to show museum detail
 app.get('/museums/:id', async (req, res) => {
     const theMuseum = await Museum.findById(req.params.id);
     res.render('museums/show',{ theMuseum });
 })
+
 
 
 // Creating a new museum
