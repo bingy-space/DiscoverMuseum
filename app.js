b7944915179853fdac34179e7c38b97f3740e481
BingNa Yang <bing334@gmail.com>
2023-01-24 20:33:52 -0500

    New EJS Tools for Layouts
    - Update app.js: setup ejs-mate
    - Create and setup views/layout/boilerplate.ejs
    - Update index.ejs, edit.ejs, new.ejs, and show.ejs template to use boilerplate

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const path = require('path');
 const mongoose = require('mongoose');
 const Museum = require('./models/museum');
 const methodOverride = require('method-override');
+const ejsMate = require('ejs-mate');
 
 // Call mongoose.connect
 mongoose.connect('mongodb://localhost:27017/discover-museum', {
@@ -19,6 +20,7 @@ db.once("open", () =>{
 
 const app = express();
 
+app.engine('ejs',ejsMate);
 app.set('view engine','ejs');
 app.set('views', path.join(__dirname, 'views'));
 // parses incoming requests
