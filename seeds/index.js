9aa86b8d7090f45676d33e1d4bf7c3ff2eca08fe
BingNa Yang <bing334@gmail.com>
2023-01-26 18:56:22 -0500

    Adding Image and price
    - Update models/museum.js: update price to Number and add image
    - Update seeds/index.js: add price and image and seed the the database
    - Update museums/show.ejs: display price, image, and description

--- seeds/index.js
+++ seeds/index.js
@@ -27,9 +27,14 @@ const seedDB = async () => {
     await Museum.deleteMany({});
     for(let i=0; i<50; i++){
         const random1000 = Math.floor(Math.random() * 1000);
+        const price = Math.floor(Math.random() * 20 + 10);
+
         const theMuseum = new Museum({
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
-            title: `${sample(name)}`
+            title: `${sample(name)}`,
+            image: 'https://images.unsplash.com/photo-1513038630932-13873b1a7f29?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=735&q=80',
+            description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
+            price
         })
         await theMuseum.save();
     }
