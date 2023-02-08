1e6bd41159fee4329ab0df8237c4d3674c3ac14b
BingNa Yang <bing334@gmail.com>
2023-02-08 18:48:26 -0500

    Update models/museum.js: create middleware to delete museum with all reviews

--- models/museum.js
+++ models/museum.js
@@ -15,4 +15,14 @@ const MuseumSchema = new Schema({
     ]
 })
 
+MuseumSchema.post('findOneAndDelete', async function (doc) {
+    if(doc){
+        await Review.deleteMany({
+            _id:{
+                $in: doc.reviews
+            }
+        })
+    }
+})
+
 module.exports = mongoose.model('Museum', MuseumSchema)
